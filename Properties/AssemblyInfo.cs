8dd8afda591bdc40efb5c9513e49e1117609ecde
Imdsm <adamkdean@googlemail.com>
2013-02-16 22:46:41 +0000

    converted project to class library

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("d836763e-8d37-4066-869d-2842c67aec22")]
+[assembly: Guid("67625144-d014-4cb8-b1f9-7e1cc9478586")]
 
 // Version information for an assembly consists of the following four values:
 //
